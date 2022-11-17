5008cf721736d91ec8a4d6bae636c5ea29c5b39a
ipeerless <ipeerless0@gmail.com>
2022-11-17 19:36:49 +0200

    reactjs api axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import Contact from "./Contact";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Contact from './Contact'
+
 
 function App() {
+  const [contacts, setContacts] = useState([])
+  const [error, setError] = useState(null)
+  useEffect(() => {
+    axios('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        setContacts(response.data)
+        setError(null)
+      })
+      .catch(setError)
+  }, [])
+   if (error) return <p>An error occurred</p>
+
   return (
     <div className="App">
-      <Contact />
+      {contacts.map(({ id, name, email, company }) => (
+        <Contact
+          key={id}
+          name={name}
+          email={email}
+          tagline={company.catchPhrase}
+        />
+      ))}
     </div>
   );
 }
 
 export default App;
+
+
